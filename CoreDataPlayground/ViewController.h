913ca3ce67c157276d08b3f9d01026065ffe99ee
Adam Dunn <dunnadam@gmail.com>
2014-12-11 14:00:37 -0700

    Now will insert/save and pull entities from store. Using dependency injection to hand off managedObjectContext from AppDelegate to ViewController so that we can do actions in buttonClick action outlet. No longer following guide from objc.io

--- CoreDataPlayground/ViewController.h
+++ CoreDataPlayground/ViewController.h
@@ -9,7 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 
+- (IBAction)buttonClicked:(UIButton *)sender;
 
 @end
 
--- CoreDataPlayground/ViewController.m
+++ CoreDataPlayground/ViewController.m
@@ -8,12 +8,16 @@
 
 #import "ViewController.h"
 #import <CoreData/CoreData.h>
+#import "Item.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *OutputText;
 
 @end
 
 @implementation ViewController
+
+@synthesize managedObjectContext;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,6 +27,39 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonClicked:(UIButton *)sender {
+    NSMutableString *mutatingString = [[NSMutableString alloc] init];
+    Item *newItem = [NSEntityDescription
+                                insertNewObjectForEntityForName:@"Item"
+                                inManagedObjectContext:managedObjectContext];
+    newItem.title = @"My title";
+    newItem.order = [NSNumber numberWithInt:2];
+    [managedObjectContext save:NULL];
+    
+    [mutatingString appendString:newItem.title];
+    _OutputText.text = mutatingString;
+    
+    NSEntityDescription *entityDescription = [NSEntityDescription
+                                              entityForName:@"Item" inManagedObjectContext:managedObjectContext];
+    NSFetchRequest *request = [[NSFetchRequest alloc]init];
+    [request setEntity:entityDescription];
+    
+    NSError *error;
+    NSArray *array = [managedObjectContext executeFetchRequest:request error:&error];
+    if (array == nil) {
+        [mutatingString appendString:@"\nthere was an error retrieving"];
+        _OutputText.text = mutatingString;
+    }
+    else
+    {
+        for (Item *anItem in array) {
+            [mutatingString appendFormat:@"\ngot the object:%@", anItem.title];
+            _OutputText.text = mutatingString;
+        }
+        
+    }
 }
 
 @end
